5b5d2c381c891794d1682a3305a01dd01ff1d564
Sandra11 <sandra.wackerl@vodafone.de>
2020-11-27 14:52:35 +0100

    Uebungsblatt2-Gruppenaufgabe

---
+++ index2.js
@@ -0,0 +1,8 @@
+function textChange(){
+    var newText1 = document.getElementById("textfeld1").value;
+    document.getElementById("myText").innerHTML = newText1;
+
+    var newText2 = document.getElementById("textfeld2").value;
+    document.getElementById("myText2").innerHTML = newText2;
+
+}
