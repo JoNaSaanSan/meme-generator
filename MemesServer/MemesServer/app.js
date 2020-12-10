05b834aec5e6f4a1d0cb3a6bb514fd6176c2069c
Florian Peste <peste@cip.ifi.lmu.de>
2020-12-10 22:43:12 +0100

    saving memes on server

--- MemesServer/MemesServer/app.js
+++ MemesServer/MemesServer/app.js
@@ -8,6 +8,7 @@ var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var samplememesRouter = require('./routes/samplememes');
+var savememeRouter = require('./routes/savememe');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/samplememes', samplememesRouter);
+app.use('/savememe', savememeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ MemesServer/MemesServer/routes/savememe.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+savedmemes = []
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+  var url = req.query.url;
+  savedmemes.push(url);
+  res.send("Meme saved!")
+
+});
+
+module.exports = router;
