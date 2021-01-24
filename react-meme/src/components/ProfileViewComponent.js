6ac9a33e770050b8f1e4cbd896b85efd4a8cce9b
Sandra11 <sandra.wackerl@vodafone.de>
2021-01-24 23:30:46 +0100

    profile interface done
    without backend

--- react-meme/src/components/ProfileViewComponent.js
+++ react-meme/src/components/ProfileViewComponent.js
@@ -1,6 +1,8 @@
-
+import profilepic from '../profilepic.png'
+import PreviewComponent from './PreviewComponent';
 const React = require('react');
 require('./ProfileViewComponent.css');
+
 
 class ProfileViewComponent extends React.Component {
 
@@ -11,10 +13,27 @@ class ProfileViewComponent extends React.Component {
   render() {
 
     return (
-
-      <div>
-     
+    <div className= "container_all">
+      
+      <div className= "container_profile-settings">
+        <img className="profile-picture" src={profilepic} />
+        <div className="container_nickname">
+          <p className="input-label">Nickname</p>
+          <input className="input-nickname"/>
+        </div>
       </div>
+
+      <div className= "container_right">
+        <div className= "container_memes">
+          <h2 className="title">Generated Images</h2>
+        </div>
+
+        <div className= "container_memes">
+          <h2 className="title">Favourite Images</h2>
+        </div>
+      </div>
+
+    </div>
     )
   }
 }
