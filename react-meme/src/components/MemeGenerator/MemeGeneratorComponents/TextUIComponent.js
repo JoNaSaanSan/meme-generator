e997c25f7e907820dc6cbb68f2700267f4090768
Jonathan <Jonathan.Haudenschild@gmail.com>
2021-02-15 15:05:50 +0100

    added text ui

--- react-meme/src/components/MemeGenerator/MemeGeneratorComponents/TextUIComponent.js
+++ react-meme/src/components/MemeGenerator/MemeGeneratorComponents/TextUIComponent.js
@@ -32,8 +32,9 @@ class TextUIComponent extends React.Component {
     if (this.props.currentInputBoxes !== null && this.props.currentInputBoxes !== undefined) {
       return this.props.currentInputBoxes.map((el, i) =>
         <div key={i} className="text-item">
+           <div>Text</div>
           <input type="text" id={"text-input_"+i}  placeholder="Text" name="text" value={this.props.currentInputBoxes[i].text} className="input-box" onChange={this.handleChange.bind(this, i)} />
-          <div className="hide">
+            <div>Font</div>
             <input type="text" placeholder="50" name="fontSize" value={this.props.currentInputBoxes[i].fontSize} className="number-input-box" min="1" max="100" maxLength="2" onChange={this.handleChange.bind(this, i)} />
             <select name="fontFamily" className="input-box" value={this.props.currentInputBoxes[i].fontFamily} onChange={this.handleChange.bind(this, i)}>
               <option value="Impact">Impact</option>
@@ -44,11 +45,12 @@ class TextUIComponent extends React.Component {
               <option value="Verdana">Verdana</option>
             </select>
             <input type="color" name="fontColor" className="color-input-box" value={this.props.currentInputBoxes[i].fontColor} onChange={this.handleChange.bind(this, i)} />
+            <div>Outline</div>
             <input type="text" placeholder="3" name="outlineWidth" value={this.props.currentInputBoxes[i].outlineWidth} className="number-input-box" min="1" max="20" onChange={this.handleChange.bind(this, i)} />
             <input type="color" name="outlineColor" value={this.props.currentInputBoxes[i].outlineColor} className="color-input-box" onChange={this.handleChange.bind(this, i)} />
+            <div>Position</div>
             <input type="number" placeholder="200" name="textPosX" value={this.props.currentInputBoxes[i].textPosX} className="dimension-input-box" min="1" max="5000" maxLength="2" onChange={this.handleChange.bind(this, i)} />
             <input type="number" placeholder="200" name="textPosY" value={this.props.currentInputBoxes[i].textPosY} className="dimension-input-box" min="1" max="5000" maxLength="2" onChange={this.handleChange.bind(this, i)} />
-          </div>
         </div>)
     } else {
       return;
