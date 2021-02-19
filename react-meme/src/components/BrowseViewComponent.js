8df9cb7a231e82ffcc7eb338dfb5560d11df232d
Sandra11 <sandra.wackerl@vodafone.de>
2021-02-19 21:33:54 +0100

    show info from db works (besides commit)

--- react-meme/src/components/BrowseViewComponent.js
+++ react-meme/src/components/BrowseViewComponent.js
@@ -5,37 +5,61 @@ require('./BrowseViewComponent.css');
 class BrowseViewComponent extends React.Component {
   
   state = {
-    allMemes: []
+    allMemes: [],
+    popularMemes: []
   }
 
-  componentDidMount() {
+  componentWillMount() {
     this.getAllMemes(); 
+    this.getPupularMemes();
   }
 
-  createAllMemes(){
-    console.log("allMemes ausgeben :"+ this.state.allMemes);
-    if(this.state.allMemes == !undefined){
-    
+  createMemes(){
+    console.log(this.state.allMemes)
+    if(this.state.allMemes !== undefined){
     return(
-      this.state.allMemes.map((i) =>
-          <div ><img src="https://www.scinexx.de/wp-content/uploads/s/c/schrumpfeng-1.jpg"/><ImageOptionsText title={this.state.allMemes[i].title}/></div>
-          
-        )
-    )
+      this.state.allMemes.map((object, i) =>
+          <div><ImageOptionsText meme={this.state.allMemes[i]}/></div>
+      )
+    )}
+    else{
+      return (
+        <p>nicht gefunden</p>
+      )
     }
   }
 
+  /**
+   * Memes Array sorted by time of creation
+   */
   getAllMemes(){
     fetch('http://localhost:3000/memes/browsememes').then(response => {
       return response.json();
     })
     .then(data => {
-      console.log("data: " + JSON.stringify(data))
-      this.setState({allMemes: data}, () => this.createAllMemes())
+      //console.log("data: " + JSON.stringify(data))
+      this.setState({allMemes: data})//, () => this.createMemes(this.state.allMemes))
     }).catch(error => {
       console.log(error);
     });
   }
+
+  /**
+   * Memes Array sorted by number of upvotes
+   */
+  getPupularMemes(){
+    fetch('http://localhost:3000/memes/popularmemes').then(response => {
+      return response.json();
+    })
+    .then(data => {
+      //console.log("data popularmemes: " + JSON.stringify(data))
+      this.setState({popularMemes: data})//, () => this.createMemes(this.state.popularMemes))
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
+
 
 
   render() {
@@ -43,8 +67,7 @@ class BrowseViewComponent extends React.Component {
 
       <div className="imageOptions_container">
         <div id= "test">
-          {this.createAllMemes()}
-          <ImageOptionsText/>
+          {this.createMemes()}
         </div>
       </div>
     );
--- react-meme/src/components/ImageOptionsText.js
+++ react-meme/src/components/ImageOptionsText.js
@@ -12,19 +12,52 @@ class ImageOptionsText extends React.Component {
 
     upvote(){
         console.log("upvote");
+        var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MDJhZTM1OTc5OWQ4MDJkYjA4YTRjOGQiLCJpYXQiOjE2MTM0Nzg0MjEsImV4cCI6MTYxMzU2NDgyMX0.UCmL0JPFKC8QY5-cInYNDxmLDe7Qh0GpFMwcwOSVqes"
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'x-access-token': token
+            },
+            body: this.props.meme.id
+          };
+        fetch('http://localhost:3000/memes/upvote', requestOptions)
+        .then(response => {
+            return response.json();
+        })
     }
 
     downvote(){
         console.log("downvote");
     }
 
+    numberOfComments(){
+        if(this.props.meme.comments != undefined){
+            return this.props.meme.comments.length
+        }
+        else{
+            return 0
+        }
+    }
+
+    commit(){
+        console.log("make a commit")
+        return(
+            <div>Testen</div>
+        )
+    }
+
+
     render() { 
         return (
             <div className= "container">
-                <p className="username">Username</p>
+                <div className= "user-date_container">
+                    <p className="username">Username</p>
+                    <p className="date">{this.props.meme.dateCreated}</p>
+                </div>
                 <div className= "above-image_container">
                     
-                        <h1 className="image-titel">{this.props.title}</h1>
+                        <h1 className="image-titel">{this.props.meme.title}</h1>
                     
                     <div className="options-top_container">
                         <div className="share-download">
@@ -34,17 +67,22 @@ class ImageOptionsText extends React.Component {
                     </div>
                 </div>
                 <div className="image_container">
-                    <img src="https://m.media-amazon.com/images/I/51Ov85BvemL._AC_SY741_.jpg" className="image" />
+                    <img src= {this.props.meme.base64} className="image" />
                 </div>
                 <div className="points-commits">
-                    <p className= "voting-point">Points: 235 </p>
-                    <p className= "voting-point">Comments: 10</p>
+                    <p className= "voting-point">Points: {this.props.meme.upvotes + this.props.meme.downvotes} </p>
+                    <p className= "voting-point">Comments: {this.numberOfComments()}</p>
                 </div>
                 <div className="option_container">
                     <button className="option-button"><img src= {upvote} className="icon" onClick={() => this.upvote()}/></button>
                     <button className="option-button"><img src= {downvote} className="icon" onClick={() => this.downvote()}/></button>
-                    <button className="option-button"><img src= {comments} className="icon"/></button>
+                    <button className="option-button"><img src= {comments} className="icon" onClick={() => this.commit()}/></button>
+                </div>    
                     
+
+                <div className="comments">
+                    <input className="input-text"/>
+                    <button className="send">Send</button>
                 </div>
                 
                 
