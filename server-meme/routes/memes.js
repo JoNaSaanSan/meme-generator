c877e45ea0e1add4c599bb14b002602e76c319f7
Flo <f.peste@campus.lmu.de>
2021-02-28 23:34:50 +0100

    removed consoel log

--- server-meme/routes/memes.js
+++ server-meme/routes/memes.js
@@ -385,7 +385,6 @@ router.get("/:id", (req, res) => {
   memes.findOne({
     _id: id
   }).then(meme => {
-    console.log(meme);
     res.status(200).send(meme);
   }).catch(error => {
     console.log(error);
